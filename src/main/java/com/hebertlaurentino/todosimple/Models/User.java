972dd3fca7cd646fa2920df6dc8f0606df8349fa
Hebert Laurentino dos Santos <hebert.santos343@gmail.com>
2024-06-03 10:42:54 -0300

    Deleting Unused Import

--- src/main/java/com/hebertlaurentino/todosimple/Models/User.java
+++ src/main/java/com/hebertlaurentino/todosimple/Models/User.java
@@ -3,7 +3,6 @@ package com.hebertlaurentino.todosimple.Models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
